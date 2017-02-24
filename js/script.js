81c823b04cbdd50f9440db0664b5866bbe3561e2
Jana McKinnon <janamck007@gmail.com>
2017-02-24 15:29:28 -0500

    updated template page and edited FAQ links, edited script file

--- js/script.js
+++ js/script.js
@@ -6,3 +6,45 @@ $(".timer")
       event.strftime('%D | %H | %M | %S')
     );
 });
+
+/* Time line */
+(function() {
+
+  'use strict';
+
+  // define variables
+  var items = document.querySelectorAll(".timeline li");
+
+  // check if an element is in viewport
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
+      }
+    }
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+
+})();
+
+
+/* toggle expandable divs */
+
+ $('.expand').click(function(){
+            $(this).next('.expandcontent').slideToggle('slow');
+});
